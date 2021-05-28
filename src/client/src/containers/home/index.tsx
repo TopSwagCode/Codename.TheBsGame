7ebb184f3e29ea7ddefd70a6408b21c13950c8f5
mikkel@nhorn.dk <mikkel@nhorn.dk>
2021-05-28 22:17:24 +0200

    bigger link size

--- src/client/src/containers/home/index.tsx
+++ src/client/src/containers/home/index.tsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom'
 class HomeContainer extends PureComponent<Record<string, never>> {
 	render = (): JSX.Element => {
 		return (
-			<div className="home">
-				<Link to="/game">Game</Link>
+			<div className="home" style={{ padding: 100 }}>
+				<Link to="/game" style={{ fontSize: 50 }}>
+					Game
+				</Link>
 			</div>
 		)
 	}
