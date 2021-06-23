4e44eab94434fc68a66d8aafff1979fcc224988f
Philip <philipkristoffersen@gmail.com>
2021-06-23 05:58:15 +0200

    Initial HelloWorld wasm crate

---
+++ server/wasm/src/lib.rs
@@ -0,0 +1,13 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    unsafe {
+        alert(&format!("Hello, {}!", name));
+    }
+}
